8b306e0f2d7664da0a5a9a1c2c1de18588b554c4
joejoequte <93330219+joejoequte@users.noreply.github.com>
2023-09-16 20:13:24 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from whatisthis import t683531
+
+if __name__ == '__main__':
+    apps = [
+        1766060
+    ]
+    for el in apps:
+        print(f"\n======================={el}==============================\n")
+        t683531(el)
